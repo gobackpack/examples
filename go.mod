a02fb96fe03be319368d2b36ae4e6a03a34ed782
Semir Mahovkic <semirm.dev@gmail.com>
2021-04-23 12:47:28 +0200

    gobackpack

--- go.mod
+++ go.mod
@@ -6,9 +6,11 @@ require (
 	github.com/confluentinc/confluent-kafka-go v1.6.1
 	github.com/gin-gonic/gin v1.7.1
 	github.com/gobackpack/rmq v0.0.0-20210421075736-ac44fa8030bb
+	github.com/gobackpack/websocket v0.0.0-20210421082358-eec6a8b4e522 // indirect
 	github.com/golang/protobuf v1.5.2
 	github.com/google/uuid v1.2.0
 	github.com/sirupsen/logrus v1.8.1
 	github.com/streadway/amqp v1.0.0
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
 	google.golang.org/protobuf v1.26.0
 )
---
+++ ws/client/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gobackpack/websocket"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	client := &websocket.Client{
+		MessageHandler: &mHandler{},
+	}
+
+	done := make(chan bool)
+	ready := make(chan bool)
+
+	go client.Connect(done, ready, "ws://localhost:8080")
+
+	<-ready
+
+	// ready to send messages to websocket channel
+	for i := 0; i < 10000; i++ {
+		if err := client.SendText([]byte(fmt.Sprint("message: ", i))); err != nil {
+			logrus.Fatal("failed to send message: ", err)
+		}
+	}
+
+	close(done)
+
+	<-done
+}
+
+// mHandler example impl
+type mHandler struct{}
+
+func (h *mHandler) OnMessage(in []byte, reply func(int, []byte) error) {}
+
+func (h *mHandler) OnError(err error) {
+	logrus.Error("error from ws connection: ", err)
+}
